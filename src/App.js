998c10671cd6249ae3ba02828aed16a0df6bc108
Elmi Rabelo <rabeloelmi@gmail.com>
2019-09-09 23:07:36 -0300

    Adicionado Spinner Component, State da aplicação movido para App Component, conectando ao github api para obter usuários, e adicionado condicional para renderizar spinner enquanto faz o fetch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 
+import axios from "axios";
+
 //components
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
@@ -7,12 +9,28 @@ import Users from "./components/users/Users";
 import "./App.css";
 
 class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			users: [],
+			carregando: false
+		};
+	}
+
+	async componentDidMount() {
+		this.setState({ carregando: true });
+
+		const res = await axios.get("https://api.github.com/users");
+
+		this.setState({ users: res.data, carregando: false });
+	}
+
 	render() {
 		return (
 			<div className="App">
 				<Navbar titulo="Perfil no Github" icon="fab fa-github" />
 				<div className="container">
-					<Users />
+					<Users carregando={this.state.carregando} users={this.state.users} />
 				</div>
 			</div>
 		);
--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -1,49 +1,32 @@
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 
+import Spinner from "../layout/Spinner";
 import UserItem from "./Useritem";
 
-class Users extends Component {
-	constructor() {
-		super();
-		this.state = {
-			users: [
-				{
-					login: "mojombo",
-					id: 1,
-					avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-					html_url: "https://github.com/mojombo"
-				},
-				{
-					login: "defunkt",
-					id: 2,
-					avatar_url: "https://avatars0.githubusercontent.com/u/2?v=4",
-					html_url: "https://github.com/defunkt"
-				},
-				{
-					login: "pjhyett",
-					id: 3,
-					avatar_url: "https://avatars0.githubusercontent.com/u/3?v=4",
-					html_url: "https://github.com/pjhyett"
-				}
-			]
-		};
-	}
-
-	render() {
+const Users = ({ users, carregando }) => {
+	if (!carregando) {
 		return (
 			<div style={userStyles}>
-				{this.state.users.map(user => (
+				{users.map(user => (
 					<UserItem key={user.id} user={user} />
 				))}
 			</div>
 		);
+	} else {
+		return <Spinner />;
 	}
-}
+};
 
 const userStyles = {
 	display: "grid",
 	gridTemplateColumns: "repeat(3, 1fr)",
 	gridGap: "1rem"
+};
+
+Users.propTypes = {
+	users: PropTypes.array.isRequired,
+	carregando: PropTypes.bool.isRequired
 };
 
 export default Users;
