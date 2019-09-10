9cf31e7a7e76235652ce7ff0ebf2b677d85adf27
Elmi Rabelo <rabeloelmi@gmail.com>
2019-09-09 21:52:12 -0300

    Componentes foram refatorados, components sem State agora são Function Components

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,29 +1,26 @@
-import React, { Component } from "react";
+import React from "react";
 
 //é como um checador de tipos, informando que tipo de dado sua prop deve possuir
 import PropTypes from "prop-types";
 
-class Navbar extends Component {
-	static defaultProps = {
-		titulo: "Perfil no Github",
-		icon: "fab fa-github"
-	};
+const Navbar = ({ titulo, icon }) => {
+	return (
+		<nav className="navbar bg-primary">
+			<h1>
+				<i className={icon}></i> {titulo}
+			</h1>
+		</nav>
+	);
+};
 
-	static propTypes = {
-		titulo: PropTypes.string.isRequired,
-		icon: PropTypes.string.isRequired
-	};
+Navbar.defaultProps = {
+	titulo: "Perfil no Github",
+	icon: "fab fa-github"
+};
 
-	render() {
-		const { titulo, icon } = this.props;
-		return (
-			<nav className="navbar bg-primary">
-				<h1>
-					<i className={icon}></i> {titulo}
-				</h1>
-			</nav>
-		);
-	}
-}
+Navbar.propTypes = {
+	titulo: PropTypes.string.isRequired,
+	icon: PropTypes.string.isRequired
+};
 
 export default Navbar;
